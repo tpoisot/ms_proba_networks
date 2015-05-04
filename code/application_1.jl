9f8a9793e4d0251e13a80408add73096267f5eef
Timothée Poisot <tim@poisotlab.io>
2015-05-04 14:46:47 -0400

    Motifs

--- code/application_1.jl
+++ code/application_1.jl
@@ -16,9 +16,21 @@ distr_nest = [A |> make_bernoulli |> nestedness for i in 1:1000]
 mapslices(mean, hcat(distr_nest...), 2)
 
 # Motif 1
-m1 = [0.0 1.0 1.0; 0.0 0.0 0.0; 0.0 0.0 0.0]
-println(motif(make_unipartite(A), m1))
+b1 = make_unipartite([1.0 0.0; 0.0 1.0])
+b2 = make_unipartite([1.0 1.0; 0.0 1.0])
+b3 = make_unipartite([1.0 1.0; 1.0 1.0])
+
+uA = make_unipartite(A)
+println("PROBA")
+println("b1:", motif(uA, b1), " b2:", motif(uA, b2), " b3:", motif(uA, b3))
+
+uA = make_binary(A)
+println("BINARY")
+println("b1:", motif(uA, b1), " b2:", motif(uA, b2), " b3:", motif(uA, b3))
+
+
+#println(motif(make_unipartite(A), m1))
 
 # Bernoulli trials
-nm1 = map((x) -> motif(make_unipartite(x), m1), nullmodel(A, n=50))
-nm1prime = map((x) -> motif(make_unipartite(x), m1'), nullmodel(A, n=50))
+#nm1 = map((x) -> motif(make_unipartite(x), m1), nullmodel(A, n=50))
+#nm1prime = map((x) -> motif(make_unipartite(x), m1'), nullmodel(A, n=50))
